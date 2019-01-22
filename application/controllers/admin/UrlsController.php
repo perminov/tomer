a8347721ea4efd939322bf63d2cd2e9f3fa4a87e
Pavel Perminov <pavel.perminov.23@gmail.com>
2019-01-23 00:07:59 +0300

    trying to setup curl proxy

--- application/controllers/admin/UrlsController.php
+++ application/controllers/admin/UrlsController.php
@@ -5,8 +5,44 @@ class Admin_UrlsController extends Indi_Controller_Admin {
      * Do parse
      */
     public function parseAction() {
-    
-        // Flush temporary msg
-        jflush(false, 'Not yet implemented');
+
+        // Create curl session
+        $curl = curl_init();
+
+        // Get Usage-model shortcut
+        $model = Indi::model('Usage');
+
+        // Get `usage` entry, that either have never been used, or having most older usage datetime
+        if (!$proxy = $model->fetchRow(['`usedAt` = "0000-00-00 00:00:00"', '`toggle` = "y"'], 'RAND()'))
+            $proxy = $model->fetchRow(null, '`usedAt` ASC');
+
+        // Prepare curl session options
+        $optA = [
+            CURLOPT_URL => $this->row->title,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HEADER => true,
+            //CURLOPT_HTTPPROXYTUNNEL => true,
+            //CURLOPT_PROXY => $proxy->title,
+            //CURLOPT_PROXYTYPE => CURLPROXY_SOCKS5
+        ];
+
+        // Setup curl session options
+        curl_setopt_array($curl, $optA);
+
+        // Fetch response
+        $response = curl_exec($curl);
+
+        // Info about what proxy was used
+        $info = 'Proxy: ' . $proxy->title . "\n";
+
+        // If response is boolean false - return curl error
+        if ($response === false) jflush(false, nl2br($info) . 'curl_exec() === false: ' . curl_error($curl));
+
+        // Setup last usage datetime
+        if ($optA[CURLOPT_PROXY]) $proxy->assign(['usedAt' => date('Y-m-d H:i:s')])->save();
+
+        // Flush response
+        jflush(true, '<textarea style="width: 500px; height: 400px;">' . $info . $response . '</textarea>');
     }
 }
